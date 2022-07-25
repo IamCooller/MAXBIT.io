43a5619e8f758138c9522fa6dfe246c86e6b7023
IamCooller <cooller2000@inbox.ru>
2022-07-25 22:09:02 +0300

    title

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,15 @@
 //Parallax
 
+jQuery.browser = {};
+(function() {
+    jQuery.browser.msie = false;
+    jQuery.browser.version = 0;
+    if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
+        jQuery.browser.msie = true;
+        jQuery.browser.version = RegExp.$1;
+    }
+})();
+
 const scene = document.getElementById('scene_intro');
 const parallaxInstance = new Parallax(scene);
 
@@ -8,6 +18,47 @@ const ParallaxItems = document.querySelectorAll('.parallax');
 for (const item of ParallaxItems) {
     const parallaxInstance = new Parallax(item);
 }
+
+$('.parallax-intro').scrollingParallax({
+    loopIt: false,
+    staticScrollLimit: true,
+    staticSpeed: .5,
+    bgHeight: '100%',
+
+    disableIE6Animation: true
+});
+
+
+
+/* 
+$(function() {
+
+    $.scrollingParallax('../img/intro/bg-2.png', {
+        loopIt: true,
+        staticScrollLimit: false,
+        bgHeight: '100%',
+        disableIE6: true,
+        bgWidth: 'auto',
+        staticSpeed: .20,
+    }); */
+
+/*  $.scrollingParallax('img/leaves-midground.png', {
+     staticSpeed : .8,
+     loopIt : true,
+     bgHeight : '200%',
+     disableIE6 : true
+ });
+ 
+ $.scrollingParallax('img/leaves-background.png', {
+     staticSpeed : .17,
+     loopIt : true,
+     bgHeight : '280%',
+     disableIE6Animation : false
+ });
+  */
+/* }); */
+
+
 
 
 // AOS
@@ -183,7 +234,7 @@ $(function() {
 
 //Title
 
-{
+/* {
     const chars = ['$', '%', '#', '@', '&', '(', ')', '=', '*', '/'];
     const charsTotal = chars.length;
     const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
@@ -252,8 +303,114 @@ $(function() {
     sections.forEach(section => allentries.push(new Entry(section)));
 
 }
+ */
+{
+    const chars = ['$', '%', '#', '@', '&', '(', ')', '=', '*', '/'];
+    const charsTotal = chars.length;
+    const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+    class Entry {
+        constructor(el) {
+            this.DOM = { el: el };
+
+            this.DOM.title = { word: this.DOM.el.querySelector('.content__text') };
+
+            charming(this.DOM.title.word);
+            this.DOM.title.letters = Array.from(this.DOM.title.word.querySelectorAll('span')).sort(() => 0.5 - Math.random());
+            this.DOM.title.letters.forEach(letter => letter.dataset.initial = letter.innerHTML);
+            this.lettersTotal = this.DOM.title.letters.length;
+            observer.observe(this.DOM.el);
+        }
+        enter(direction = 'down') {
+            this.DOM.title.word.style.opacity = 1;
+
+            this.timeouts = [];
+            this.complete = false;
+            let cnt = 0;
+            this.DOM.title.letters.forEach((letter, pos) => {
+                const timeout = setTimeout(() => {
+                    letter.innerHTML = chars[getRandomInt(0, charsTotal - 1)];
+                    setTimeout(() => {
+                        letter.innerHTML = letter.dataset.initial;
+                        ++cnt;
+                        if (cnt === this.lettersTotal) {
+                            this.complete = true;
+                        }
+                    }, 100);
+                }, pos * 80);
+                this.timeouts.push(timeout);
+            });
+        }
+        exit(direction = 'down') {
+            this.DOM.title.word.style.opacity = 0;
+            if (this.complete) return;
+            for (let i = 0, len = this.timeouts.length; i <= len - 1; ++i) {
+                clearTimeout(this.timeouts[i]);
+            }
+        }
+    }
+
+    let observer;
+    let current = -1;
+    let allentries = [];
+    const sections = Array.from(document.querySelectorAll('.content__section'));
+    // Preload all the images in the page..
+    if ('IntersectionObserver' in window) {
+        document.body.classList.add('ioapi');
+
+        observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.intersectionRatio > 0.5) {
+                    const newcurrent = sections.indexOf(entry.target);
+                    if (newcurrent === current) return;
+                    const direction = newcurrent > current;
+                    if (current >= 0) {
+                        allentries[current].exit(direction ? 'down' : 'up');
+                    }
+                    allentries[newcurrent].enter(direction ? 'down' : 'up');
+                    current = newcurrent;
+                }
+            });
+        }, { threshold: 0.5 });
+
+        sections.forEach(section => allentries.push(new Entry(section)));
+    }
 
 
+    document.addEventListener("scroll", function() {
+        const scrolled = window.pageYOffset;
+        let content__text = document.querySelector('.content__text');
+        let FirstBlock = document.querySelectorAll(".content__text")[0];
+        let lastBlock = document.querySelectorAll(".content__text")[document.querySelectorAll(".content__text").length - 1];
+        let FooterHeight = document.querySelector(".footer").offsetHeight;
+        let InnerBlock = document.querySelector(".intro");
+
+
+        const observer = new window.IntersectionObserver(([entry]) => {
+            if (entry.isIntersecting) {
+                FirstBlock.style.bottom = '-20vh';
+                return
+            }
+            FirstBlock.style.bottom = '6vmax';
+        }, {
+            root: null,
+            threshold: 0.5, // set offset 0.1 means trigger if atleast 10% of element in viewport
+        })
+
+        observer.observe(InnerBlock);
+
+
+
+        if ($(window).scrollTop() + $(window).height() + FooterHeight >= $(document).height()) {
+
+            lastBlock.style.left = '-100vh';
+
+        } else {
+            lastBlock.style.left = '0';
+        }
+    });
+
+}
 //Script for load elements
 
 function waitForElm(selector) {
